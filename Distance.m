59041b8f0fbb600f250dc903266d0668951a4983
ElSarik <sarikakisilias@gmail.com>
2020-12-23 10:09:54 +0200

    Distance hotfix?
    Changed the equation of Distance.m

--- Distance.m
+++ Distance.m
@@ -3,7 +3,8 @@
 function dist = Distance(Wk, Wk_Plus1)
     dist = 0;
     for i=1:size(Wk)
-        dist = dist + ((Wk(1,i)-Wk_Plus1(1,i)).^2).^(1/2);
-%         disp(dist);
+        dist = dist + (Wk(1,i)-(Wk_Plus1(1,i))).^2;
     end
+    dist = dist.^(1/2);
+%     disp(dist);
 end
