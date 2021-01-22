adebd609e6d3c5f9a017d16512d8d91686f612d3
ElSarik <sarikakisilias@gmail.com>
2021-01-22 13:35:36 +0200

    Distance check for every pattern
    - Added distance check for every pattern

--- PerceptronV2.m
+++ PerceptronV2.m
@@ -7,6 +7,7 @@ function p = PerceptronV2(X1,X2,X3,Y,W1,W2,W3,W0)
     Epoch_completed = 0;
     
     epsilon = 0.01;
+    distance = epsilon;
     Wk = [W0, W1, W2, W3];  % epsilon, Wk, and Wk_Plus1 initialization
     Wk_Plus1 = zeros(size(Wk));
 %% Perceptron Algorithm
@@ -26,6 +27,10 @@ function p = PerceptronV2(X1,X2,X3,Y,W1,W2,W3,W0)
                     W1 = W1 + LR*-1*X1(1,i); % Weight changes
                     W2 = W2 + LR*-1*X2(1,i); % We were expecting a negative output, but
                     W3 = W3 + LR*-1*X3(1,i); % we got a positive perceptron output. *(-1)
+                    
+                    Wk_Plus1=[W0,W1,W2,W3]; % Wk_Plus1 gets updated with the new weights
+                    distance = Distance(Wk, Wk_Plus1);  % The distance between the old and the new
+                                                        % weights gets calculated
                 end
             elseif P_output <= 0 % Negative perceptron output
                 
@@ -35,22 +40,36 @@ function p = PerceptronV2(X1,X2,X3,Y,W1,W2,W3,W0)
                     W1 = W1 + LR*1*X1(1,i); % Weight changes
                     W2 = W2 + LR*1*X2(1,i); % We were expecting a positive output, but
                     W3 = W3 + LR*1*X3(1,i); % we got a negative perceptron output. *1
+                
+                    Wk_Plus1=[W0,W1,W2,W3]; % Wk_Plus1 gets updated with the new weights
+                    distance = Distance(Wk, Wk_Plus1);  % The distance between the old and the new
+                                                        % weights gets calculated
                 end
             end
+
+            if(distance < epsilon)  % End condition
+                fprintf('Second Perceptron finished in %d epochs.\n', Epoch_completed);  % Display of completed Epochs
+                delete(p);      % Removing and updating the plane with the final weights
+                p=Decision(W1,W2,W3,W0);
+                return;
+            else
+                Wk = Wk_Plus1;  % The old weights get updated with the newer weights
+            end
+            
         end
         
-        Wk_Plus1=[W0,W1,W2,W3]; % Wk_Plus1 gets updated with the new weights
+%         Wk_Plus1=[W0,W1,W2,W3]; % Wk_Plus1 gets updated with the new weights
         distance = Distance(Wk, Wk_Plus1);  % The distance between the old and the new
                                             % weights gets calculated
                                             
-        if(distance < epsilon)  % End condition
-            fprintf('Second Perceptron finished in %d epochs.\n', Epoch_completed);  % Display of completed Epochs
-            delete(p);      % Removing and updating the plane with the final weights
-            p=Decision(W1,W2,W3,W0);
-            return;
-        else
-            Wk = Wk_Plus1;  % The old weights get updated with the newer weights
-        end
+%         if(distance < epsilon)  % End condition
+%             fprintf('Second Perceptron finished in %d epochs.\n', Epoch_completed);  % Display of completed Epochs
+%             delete(p);      % Removing and updating the plane with the final weights
+%             p=Decision(W1,W2,W3,W0);
+%             return;
+%         else
+%             Wk = Wk_Plus1;  % The old weights get updated with the newer weights
+%         end
         
         Epoch = Epoch - 1;    % Epoch reduction after its completion
         Epoch_completed = Epoch_completed + 1;
